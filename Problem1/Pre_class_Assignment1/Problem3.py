6e982de46f6a9f38c8ab26d069f89f78507c8930
PantheraXpoint <48640044+PantheraXpoint@users.noreply.github.com>
2021-03-18 17:35:32 +0700

    Update

--- Problem1/Pre_class_Assignment1/Problem3.py
+++ Problem1/Pre_class_Assignment1/Problem3.py
@@ -1,8 +1,28 @@
 import numpy as np
-THICKNESS = 0.00008
-folded_thickness = THICKNESS
-for i in range(1,44):
-    folded_thickness *= 2
-   
-    
-print("Thickness: {} meters".format(folded_thickness))
+import matplotlib.pyplot as plt
+def calculation(sprinkle_obj_volume,fillup_obj_volume):
+    sprinkle_obj = []
+    fillup_obj = []
+    doubling_times = 0
+    sprinkle_obj.append(sprinkle_obj_volume)
+    fillup_obj.append(fillup_obj_volume)
+    sprinkle = sprinkle_obj_volume
+    while (sprinkle < fillup_obj_volume):
+        sprinkle *= 2
+        doubling_times += 1
+        fillup_obj.append(fillup_obj_volume)
+        sprinkle_obj.append(sprinkle)
+        print(sprinkle_obj)
+        print(fillup_obj)
+    return fillup_obj,sprinkle_obj,doubling_times
+sprinkle_obj_volume = float(input("Enter the volume of the sprinkled object: (in cubic meters)"))
+fillup_obj_volume = float(input("Enter the radius of the object you want to fill up: (in cubic meters)"))
+fillup_obj,sprinkle_obj,doubling_times = calculation(sprinkle_obj_volume,fillup_obj_volume)
+how_long = (doubling_times * 5) / 60
+plt.plot(sprinkle_obj, label = "Sprinkled Object's Volume")
+plt.plot(fillup_obj, label = "Given Object's Volume")
+plt.legend(loc = 'best')
+plt.title("How Long Will It Take For A Sprinkled Object's Volume To Reach The Given Object's Volume")
+plt.ylabel("Volume(cubic meters)")
+plt.xlabel("time(minutes)")
+plt.show()
--- Problem1/Pre_class_Assignment1/Problem6.py
+++ Problem1/Pre_class_Assignment1/Problem6.py
@@ -1,21 +1,21 @@
 
 import matplotlib.pyplot as plt
-list = []
-
-THICKNESS = 0.00008
-folded_thickness = THICKNESS
-for i in range(1,44):
-    folded_thickness *= 2
-    list.append(folded_thickness)
 
 
-plt.title("thickness of folded paper")
-plt.xlabel("number of folds")
-plt.ylabel("thickness[m]")
-plt.plot(list) # Enter the variable name of the list in "List name"
-plt.show()
-
-# The time of the graph as we can see, it is 2 times increment after each folding. However in the
-# graph, we see a very dramatic change at the end of a graph, that is because the init of the graph is large.
-# So as the fold increases to 43, it reaches the standard unit in the graph which is 1e8. Before that, the scale is 
-# small compared to even the based 1 of the 1e8 unit.
+    list_n_grains = []
+    list_total_grains = []
+    grain_d = 1
+    list_n_grains.append(grain_d)
+    list_total_grains.append(grain_d)
+    for i in range(1,100):
+        grain_d *= 2
+        list_n_grains.append(grain_d)
+        list_total_grains.append(list_total_grains[i - 1] + (list_n_grains[i])
+                                 
+    plt.title("the change in the number of rice grains on x day")
+    plt.xlabel("number of days")
+    plt.ylabel("number of rice")
+    plt.plot(list_n_grains)
+    plt.plot(list_total_grains)
+    plt.show()
+    pass
